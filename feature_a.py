d11f992ea350f614d7e334a0bd73c870af3e9011
nidhitalastha <talan01@pfw.edu>
2024-03-04 22:28:09 -0500

    implements feature_a average velocity and sprint points display

--- feature_a.py
+++ feature_a.py
@@ -1,8 +1,12 @@
+from tabulate import tabulate
+
 class feature_a:
     ''' This class is responsible for collecting the data for the feature A '''
 
     def __init__(self):
         self.sprint_points = []
+        self.average_velocity = 0.0
+        self.input()
 
     def input(self):
         ''' Collect previous sprint point completion data to calculate velocity of the team '''
@@ -16,18 +20,24 @@ class feature_a:
             sprint_point = int(input("Enter the sprint points completed in sprint " + str(i+1) + ": "))
             self.sprint_points.append(sprint_point)
         print("Data collected successfully")
+        self.calculate_velocity()
 
     def calculate_velocity(self):
         ''' Calculate the average velocity of the team '''
 
         total_sprint_points = sum(self.sprint_points)
         no_of_sprints = len(self.sprint_points)
-        average_velocity = total_sprint_points / no_of_sprints
-        print("The average velocity of the team is: ", average_velocity)
+        self.average_velocity = total_sprint_points / no_of_sprints
+        self.display_output()
+
+    def display_output(self):
+        ''' Display the output of the feature A '''
+        index = [i for i in range(1, len(self.sprint_points)+1)]
+        sprint_details = zip(index, self.sprint_points)
+        print(tabulate(sprint_details, headers=["Sprint", "Sprint Points"], tablefmt="grid"))
+        print("The average velocity of the team is: ", self.average_velocity)
         
 if __name__ == "__main__":
     ''' This is the main function which will be called when the file is executed '''
     ins_a = feature_a()
-    ins_a.input()
-    ins_a.calculate_velocity()
         
