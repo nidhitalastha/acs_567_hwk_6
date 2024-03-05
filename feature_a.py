bd45043b1314f29ee2ad5f1c5aaf9a756270bc4c
nidhitalastha <talan01@pfw.edu>
2024-03-04 22:13:25 -0500

    implements feature_a average velocity calculation

--- feature_a.py
+++ feature_a.py
@@ -16,9 +16,18 @@ class feature_a:
             sprint_point = int(input("Enter the sprint points completed in sprint " + str(i+1) + ": "))
             self.sprint_points.append(sprint_point)
         print("Data collected successfully")
+
+    def calculate_velocity(self):
+        ''' Calculate the average velocity of the team '''
+
+        total_sprint_points = sum(self.sprint_points)
+        no_of_sprints = len(self.sprint_points)
+        average_velocity = total_sprint_points / no_of_sprints
+        print("The average velocity of the team is: ", average_velocity)
         
 if __name__ == "__main__":
     ''' This is the main function which will be called when the file is executed '''
     ins_a = feature_a()
     ins_a.input()
+    ins_a.calculate_velocity()
         
